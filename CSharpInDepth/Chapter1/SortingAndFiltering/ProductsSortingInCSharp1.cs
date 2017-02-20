b282f68c5abed6d0c13534a9a06d0c9849ab6056
akhilnaruto <akhil.munna02@gmail.com>
2017-02-20 21:45:51 +0530

    sorting

---
+++ CSharpInDepth/Chapter1/SortingAndFiltering/ProductsSortingInCSharp1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth.Chapter1.SortingAndFiltering
+{
+  public class ProductsSortingInCSharp1
+  {
+    private string name;
+    public string Name { get { return name; } }
+
+    private decimal price;
+    public decimal Price { get { return price; } }
+
+    public ProductsSortingInCSharp1(string name , decimal price)
+    {
+      this.name = name;
+      this.price = price;
+    }
+
+    public static ArrayList GetSampleProducts()
+    {
+      ArrayList products = new ArrayList();
+      products.Add(new ProductsSortingInCSharp1("ABCDE", 8.11m));
+      products.Add(new ProductsSortingInCSharp1("DEFGU", 8.11m));
+      products.Add(new ProductsSortingInCSharp1("HJII", 8.11m));
+      products.Add(new ProductsSortingInCSharp1("IKKK", 8.11m));
+      products.Add(new ProductsSortingInCSharp1("JJJ", 8.11m));
+
+      return products;
+    }
+
+    public static void PrintOrderedSampleProducts()
+    {
+      ArrayList products = ProductsSortingInCSharp1.GetSampleProducts();
+      products.Sort(new ProductNameComparer_1());
+      foreach(ProductsSortingInCSharp1 product in products)
+      {
+        Console.WriteLine(product.name);
+      }
+    }
+
+  }
+
+  class ProductNameComparer_1 : IComparer
+  {
+    public int Compare(object x, object y)
+    {
+      ProductsSortingInCSharp1 first = (ProductsSortingInCSharp1)x;
+      ProductsSortingInCSharp1 second = (ProductsSortingInCSharp1)y;
+
+      return first.Name.CompareTo(second.Name);
+    }
+  }
+}
---
+++ CSharpInDepth/Chapter1/SortingAndFiltering/ProductsSortingInCSharp2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth.Chapter1.SortingAndFiltering
+{
+  public class ProductsSortingInCSharp2
+  {
+    private string name;
+    public string Name { get { return name; } }
+
+    private decimal price;
+    public decimal Price { get { return price; } }
+
+    public ProductsSortingInCSharp2(string name , decimal price)
+    {
+      this.name = name;
+      this.price = price;
+    }
+
+    public static List<ProductsSortingInCSharp2> GetSampleProducts()
+    {
+      List<ProductsSortingInCSharp2> products = new List<ProductsSortingInCSharp2>();
+      products.Add(new ProductsSortingInCSharp2("dasdasd", 8.11m));
+      products.Add(new ProductsSortingInCSharp2("dsadasd", 8.11m));
+      products.Add(new ProductsSortingInCSharp2("dadadwerrsdasd", 8.11m));
+      products.Add(new ProductsSortingInCSharp2("rweyrtye", 8.11m));
+      products.Add(new ProductsSortingInCSharp2("adadsdad", 8.11m));
+
+      return products;
+    }
+
+    public static void PrintOrderedSampleProducts()
+    {
+      List<ProductsSortingInCSharp2> products = ProductsSortingInCSharp2.GetSampleProducts();
+      products.Sort(new ProductNameComparer());
+      foreach(ProductsSortingInCSharp2 product in products)
+      {
+        Console.WriteLine(product.Name);
+      }
+    }
+
+    public static void PrintOrderedProductsUsingDelegates()
+    {
+      List<ProductsSortingInCSharp2> products = ProductsSortingInCSharp2.GetSampleProducts();
+
+      products.Sort(delegate (ProductsSortingInCSharp2 first, ProductsSortingInCSharp2 second)
+      {
+        return first.Name.CompareTo(second.Name);
+      });
+    }
+  }
+
+  class ProductNameComparer : IComparer<ProductsSortingInCSharp2>
+  {
+    public int Compare(ProductsSortingInCSharp2 first, ProductsSortingInCSharp2 second)
+    {
+      return first.Name.CompareTo(second.Name);
+    }
+  }
+}
