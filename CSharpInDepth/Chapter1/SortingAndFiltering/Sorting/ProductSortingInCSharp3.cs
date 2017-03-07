d1f747240a66f4103e2a38736524f23a6edcd6f3
akhilnaruto <akhil.munna02@gmail.com>
2017-03-07 06:26:47 +0530

    Completed Sorting

---
+++ CSharpInDepth/Chapter1/SortingAndFiltering/Sorting/ProductSortingInCSharp3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpInDepth.Chapter1.SortingAndFiltering
+{
+  public class ProductSortingInCSharp3
+  {
+    public void CompareProducts()
+    {
+      List<Product> products = Product.GetSamplesProducts();
+      products.Sort((x,y) => x.Name.CompareTo(y.Name));
+      foreach (Product  product in products)
+      {
+        Console.WriteLine(product);
+      }
+    }
+
+    public void OrderProducts()
+    {
+      List<Product> products = Product.GetSamplesProducts();
+      foreach (Product product in products.OrderBy(p =>p.Name))
+      {
+        Console.WriteLine(product);
+      }
+    }
+  }
+
+  public class Product
+  {
+    public string Name { get; set; }
+
+    public static List<Product> GetSamplesProducts()
+    {
+      return new List<Product>();
+    } 
+  }
+}
